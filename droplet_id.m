b0e02ff3b558c2d18d4151c070b5babfcf59fdaf
Naveen Natesh <naveen497@gmail.com>
2020-09-11 12:38:04 -0400

    Can partition circles into objects and calculate statistics in matlab

--- droplet_id.m
+++ droplet_id.m
@@ -1,4 +1,4 @@
-
+format longG
 droplets = imread('/home/nrnatesh/shenlab/Droplet-organoid/image-analysis/input_images/T2.tif');
 % imshow(rgb);
 %[centers,radii] = imfindcircles(rgb,[45 60],'Sensitivity',1, 'EdgeThreshold',.7)
@@ -12,8 +12,18 @@ coord_array = table2array(coord_data);
 
 
 
-for i = 1:length(coord_array)
-circle(i) = drawcircle('Center',[coord_array(i,1),coord_array(i,2)],'Radius',coord_array(i,3));
-mask = createMask(circle(i));
+for i = 1:(length(coord_array)-58)
+    circle = drawcircle('Center',[coord_array(i,1),coord_array(i,2)],'Radius',coord_array(i,3));
+    mask = createMask(circle)
+    imshow(mask);
+    stats(i,1:2) = regionprops('table',mask, 'Centroid','MajorAxisLength');
 end
+stats_array = table2array(stats)
 
+
+
+
+%% for i = 1:length(circle)
+   % mask = createMask(circle(i))
+    %imshow(mask)
+%end
