cd525e16768d74002888420d7da703fe4f6ab86c
Naveen Natesh <naveen497@gmail.com>
2020-09-11 12:16:34 -0400

    Partition circles identified into separate circle objects

--- droplet_id.m
+++ droplet_id.m
@@ -1,18 +1,19 @@
-%
+
 droplets = imread('/home/nrnatesh/shenlab/Droplet-organoid/image-analysis/input_images/T2.tif');
 % imshow(rgb);
 %[centers,radii] = imfindcircles(rgb,[45 60],'Sensitivity',1, 'EdgeThreshold',.7)
 
 %h = viscircles(centers,radii);
-%%
+
 figure (1)
-imshow(droplets)
-coord_data = readtable('/home/nrnatesh/shenlab/Droplet-organoid/image-analysis/coordinates.csv')
-coord_array = table2array(coord_data)
+imshow(droplets);
+coord_data = readtable('/home/nrnatesh/shenlab/Droplet-organoid/image-analysis/coordinates.csv');
+coord_array = table2array(coord_data);
 
 
-hold on
+
 for i = 1:length(coord_array)
-circle = drawcircle('Center',[coord_array(i,1),coord_array(i,2)],'Radius',coord_array(i,3))
+circle(i) = drawcircle('Center',[coord_array(i,1),coord_array(i,2)],'Radius',coord_array(i,3));
+mask = createMask(circle(i));
 end
 
